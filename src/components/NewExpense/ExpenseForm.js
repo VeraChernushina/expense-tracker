229626eaddad3cb83b830e623f610c8732b1b6f5
Vera Chernushina <vera.chernushina@yandex.ru>
2021-12-26 00:47:05 +0300

    feat: add ExpensesFilter component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [ enteredTitle, setEnteredTitle ] = React.useState('');
   const titlerChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -27,7 +27,7 @@ const ExpenseForm = () => {
       date: new Date(enteredDate)
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
